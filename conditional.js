3a2a85a46ae98ca2745044bd2f9a3f6ecf85aea1
firdous-ab <fridausb2004@gmail.com>
2022-06-23 16:59:14 +0100

    Second commit to git

---
+++ conditional.js
@@ -0,0 +1,15 @@
+//  IF statements
+let isLoggedIn = true;
+
+if(isLoggedIn == true){
+    console.log('zuri');
+}
+
+// IF ELSE statements
+let number = -2;
+if (number > 0){
+    console.log('Number is greater than zero');
+}
+else{
+    console.log('Number is less than 0');
+}
--- life.js
+++ life.js
@@ -1,7 +1,30 @@
-let theRealGoat = "Ronaldo";
-let bestmusicianInNigeria = "Wizkid";
-var name = "tolu";
-const password = "happyday";
+let goat = "ronaldo";
+let num = 9;
+console.log(goat[2]);
+console.log(goat.length);
 
-console.log(theRealGoat)
-console.log(bestmusicianInNigeria)
+/* when using the .length feature: JavaScript starts counting from 1
+but the index always starts from zero; like above the index of r in 
+'ronaldo is 0 and that of o is 6, but the length of the whole word is 7'*/
+
+//STRING FUNCTIONS and METHOD
+
+let goatCap = goat.toUpperCase();
+let z = goat.slice(1, 7); //slice is used to return a section of the string
+console.log(z);
+
+let firstLetter = goat[0].toUpperCase();
+let result = firstLetter + z;
+console.log(result);
+
+// METHOD: Replace
+
+let message = "kola is a girl";
+
+let result1 = message.replace("kola", "tolu"); //this will replace 'kola' with 'tolu'
+console.log(result1);
+
+let city = "   lagos   ";
+console.log(city);
+console.log(city.trim()); //this function will remove or trim out unnecessary space in a string
+
