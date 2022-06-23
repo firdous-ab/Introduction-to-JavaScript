2ff44c53e419d3c172b0d4c6a33af941ee2add20
firdous-ab <fridausb2004@gmail.com>
2022-06-23 17:14:45 +0100

    second commit

--- conditional.js
+++ conditional.js
@@ -13,10 +13,25 @@ if (number > 0) {
   console.log("Number is less than 0");
 }
 
-
 let goat = "ronaldo";
 if (goat == "ronaldo") {
   console.log("Ronaldo is the real deal");
 } else {
   console.log("You are just deceiving yourselves");
+}
+
+console.log('code continues');
+
+// Nested ELSE-IF statements
+
+let username = '';
+
+if(username == ''){
+  console.log('All fields are required');
+}
+else if(username == 'Firdous'){
+    console.log('login');
+}
+else{
+    console.log('Username not recognized');
 }
