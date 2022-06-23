fd11403e3a32a2e34fd407ebb05c18ebd48b7972
firdous-ab <fridausb2004@gmail.com>
2022-06-23 17:04:15 +0100

    second commit

--- conditional.js
+++ conditional.js
@@ -1,15 +1,22 @@
 //  IF statements
 let isLoggedIn = true;
 
-if(isLoggedIn == true){
-    console.log('zuri');
+if (isLoggedIn == true) {
+  console.log("zuri");
 }
 
 // IF ELSE statements
 let number = -2;
-if (number > 0){
-    console.log('Number is greater than zero');
+if (number > 0) {
+  console.log("Number is greater than zero");
+} else {
+  console.log("Number is less than 0");
 }
-else{
-    console.log('Number is less than 0');
+
+
+let goat = "ronaldo";
+if (goat == "ronaldo") {
+  console.log("Ronaldo is the real deal");
+} else {
+  console.log("You are just deceiving yourselves");
 }
